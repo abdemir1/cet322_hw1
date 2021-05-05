b5f702c63336fad52c76cf633005d4f382ab3c65
abdemir <73543378+abdemir1@users.noreply.github.com>
2021-05-06 02:53:02 +0300

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using homework1.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
