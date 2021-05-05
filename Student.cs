074dbf88ecd60d9556abe7ea65a080cef84868ef
abdemir <73543378+abdemir1@users.noreply.github.com>
2021-05-06 02:51:40 +0300

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace homework1.Models
+{
+    public class Student
+    {
+        public Student()
+        {
+            CreationDate = DateTime.Now;
+        }
+
+
+        [DisplayName("Student ID")]
+        [Required]
+        public int Id { get; set; }
+
+        [DisplayName("Name")]
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        [DisplayName("Date of Birth")]
+        public DateTime BirthDate { get; set; }
+
+        public string Department { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime LastUpdateDate { get; set; }
+
+
+
+    }
+}
